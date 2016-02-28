e4e26527e647eee18a0fdaf89292df8d00f6c4dd
cauda964 <cauda964@newschool.edu>
2016-02-28 18:33:48 -0500

    added save and update function

--- Plain Notes/DetailViewController.swift
+++ Plain Notes/DetailViewController.swift
@@ -22,22 +22,47 @@ class DetailViewController: UIViewController {
 
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = self.detailItem {
+        if objects.count == 0 {
+            return
+        }
             if let label = self.detailDescriptionLabel {
-                label.text = detail.description
-            }
+                label.text = objects[currentIndex]
+                if label.text == BLANK_NOTE {
+                    label.text = ""
+                }
+            
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        detailViewController = self
+        detailDescriptionLabel.becomeFirstResponder()
         self.configureView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if objects.count == 0{
+            return
+        }
+        objects[currentIndex] = detailDescriptionLabel.text
+        if detailDescriptionLabel.text == ""{
+            objects[currentIndex] = BLANK_NOTE
+        }
+        
+        saveAndUpdate()
+    }
+    
+    func saveAndUpdate(){
+        masterView?.save()
+        masterView?.tableView.reloadData()
     }
 
 
--- Plain Notes/MasterViewController.swift
+++ Plain Notes/MasterViewController.swift
@@ -22,6 +22,7 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        masterView = self
         load()
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
