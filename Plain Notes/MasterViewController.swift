0c6d940573af8a0e1bacd7f7488bb68cf9395d37
cauda964 <cauda964@newschool.edu>
2016-02-28 18:19:52 -0500

    save and load persistent data

--- Plain Notes/MasterViewController.swift
+++ Plain Notes/MasterViewController.swift
@@ -8,27 +8,30 @@
 
 import UIKit
 
-class MasterViewController: UITableViewController {
+var objects: [String] = [String]()
+var currentIndex:Int = 0
+var masterView: MasterViewController?
+var detailViewController: DetailViewController?
 
-    var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
+let kNotes:String = "notes"
+let BLANK_NOTE:String = "(New Note)"
+
+class MasterViewController: UITableViewController {
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        load()
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
         self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
     }
 
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        save()
         super.viewWillAppear(animated)
     }
 
@@ -38,7 +41,7 @@ class MasterViewController: UITableViewController {
     }
 
     func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
+        objects.insert(BLANK_NOTE, atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
@@ -48,11 +51,11 @@ class MasterViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-                controller.navigationItem.leftItemsSupplementBackButton = true
+                let object = objects[indexPath.row]
+                currentIndex = indexPath.row
+                detailViewController?.detailItem = object
+                detailViewController?.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                detailViewController?.navigationItem.leftItemsSupplementBackButton = true
             }
         }
     }
@@ -70,8 +73,8 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
         return cell
     }
 
@@ -86,6 +89,17 @@ class MasterViewController: UITableViewController {
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
+    func save(){
+        NSUserDefaults.standardUserDefaults().setObject(objects, forKey: kNotes)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    func load(){
+        if let loadedData = NSUserDefaults.standardUserDefaults().arrayForKey(kNotes) as? [String]{
+            objects = loadedData
         }
     }
 
